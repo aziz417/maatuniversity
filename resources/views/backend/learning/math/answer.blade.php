dddd5e179520e4bc055f908f5b604746542f3953
aziz417 <aziz97cse@gmail.com>
2020-10-24 10:10:57 +0600

    audio add done

--- resources/views/backend/learning/math/answer.blade.php
+++ resources/views/backend/learning/math/answer.blade.php
@@ -10,15 +10,24 @@
                     <div class="single-question mb-30">
                         <div class="row">
                             <div class="col-sm-12 col-12">
-                                <div class="pb-lg-5">
-                                    <strong class="pb-lg-3">{{ ++$key }}. {{ $question->title }}</strong>
+                                <div class="pb-lg-5"><strong>{{ ++$key }}.</strong>
+                                    <strong class="pb-lg-3">
+                                        <span class="form-control-sm" id="audio-{{ $question->id }}"
+                                              onclick="playAudio({{ $question->id }})">
+                                            <i id="changeIcon-{{ $question->id }}" class="fa fa-volume-up"></i>
+                                        </span>
+                                        {{ $question->title }}
+                                    </strong>
                                 </div>
                                 @foreach($question->options as $option)
                                     @if($option->option)
-                                    <span class="btn btn-warning" id="option-{{ $option->id }}" onclick="correctAnswer({{ $option->id }})">{{ ucfirst(@$option->option) }}</span>
+                                        <span class="btn btn-warning" id="option-{{ $option->id }}"
+                                              onclick="correctAnswer({{ $option->id }})">{{ ucfirst(@$option->option) }}</span>
                                     @endif
                                     @if($option->optionImage)
-                                        <span class="btn btn-warning mx-2" style="border: 2px solid #ffc107!important; padding: 2px!important;" id="option-{{ $option->id }}" onclick="correctAnswer({{ $option->id }})">
+                                        <span class="btn btn-warning m-2"
+                                              style="border: 2px solid #ffc107!important; padding: 2px!important;"
+                                              id="option-{{ $option->id }}" onclick="correctAnswer({{ $option->id }})">
                                             <img src="{{ Storage::disk('public')->url('options/').$option->optionImage }}">
                                         </span>
                                     @endif
@@ -27,35 +36,53 @@
                         </div>
                     </div>
                 @endforeach
-                     @php $lesson_id = Session::get('lesson_id') @endphp
-                    <a class="btn btn-warning float-right" href="{{ route('summery.index', $lesson_id) }}">Your study summary </a>
+                @php $lesson_id = Session::get('lesson_id') @endphp
+                <a class="btn btn-warning float-right" href="{{ route('summery.index', $lesson_id) }}">Your study
+                    summary </a>
 
-              </div>
+            </div>
         </div>
 
     </div>
 @endsection
 @push('script')
     <script>
+        function playAudio(e) {
 
-        function correctAnswer(e){
+            var id = '#audio-' + e;
+            /*var changeIcon = '#changeIcon-' + e;
+            //console.log($(id).find(i))
+            $(changeIcon).removeClass('fa-volume-up');
+            $(changeIcon).addClass('fa-play-circle');*/
+
+            var text = $(id).parent().text();
+
+            if ('speechSynthesis' in window) {
+                var msg = new SpeechSynthesisUtterance(text);
+                window.speechSynthesis.speak(msg);
+
+            }
+
+        }
+
+        function correctAnswer(e) {
             var data = e;
-            var id = '#option-'+e;
+            var id = '#option-' + e;
             $(id).removeClass('btn-warning')
             $(id).addClass('btn-success')
             $(id).closest("div").css({"pointer-events": "none", "opacity": "0.7"})
-            $.get('/dashboard/question/answer/', { data : data }, function (response){
-                if (response == 'correct'){
-                    $(".message").removeClass('text-danger').addClass('text-success').css('display','block').html('Welcome this answer is correct!');
-                    setTimeout(function (){
+            $.get('/dashboard/question/answer/', {data: data}, function (response) {
+                if (response == 'correct') {
+                    $(".message").removeClass('text-danger').addClass('text-success').css('display', 'block').html('Welcome this answer is correct!');
+                    setTimeout(function () {
                         $(".message").css('display', 'none');
                     }, 3000)
-                }else{
-                    var correctAnsId = '#option-'+response;
+                } else {
+                    var correctAnsId = '#option-' + response;
                     $(correctAnsId).removeClass('btn-warning').addClass('btn-success');
                     $(id).addClass('btn-danger')
-                    $(".message").removeClass('text-success').addClass('text-danger').css('display','block').html('Oops this answer is Wrong!');
-                    setTimeout(function (){
+                    $(".message").removeClass('text-success').addClass('text-danger').css('display', 'block').html('Oops this answer is Wrong!');
+                    setTimeout(function () {
                         $(".message").css('display', 'none');
                     }, 3000)
                 }
